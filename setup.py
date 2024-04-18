b35fccf45c42539eeb4f531a16b7ddeda9b5002f
Mrinal Mishra <mrinal.mishra@northwestern.edu>
2024-04-18 13:52:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='average_methylation',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Mrinal Mishra',
+    author_email='mrinal.mishra@northwestern.edu',
     description='A package to calculate average methylation scores.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
